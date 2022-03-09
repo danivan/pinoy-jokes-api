5f92d2d4a413249577ffdba2e2d3732f71b2809e
Ivan Sentillas <dan.ivan.sentillas@gmail.com>
2022-03-10 01:41:01 +0800

    build: setup express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log('Hello world!');
+import express, { Application, Request, Response } from "express";
+
+const app: Application = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get(
+    "/",
+    async (req: Request, res: Response): Promise<Response> => {
+        return res.status(200).send({
+            message: "Hello World!",
+        });
+    }
+);
+
+try {
+    app.listen(port, (): void => {
+        console.log(`Connected successfully on port ${port}`);
+    });
+} catch (error: any) {
+    console.error(`Error occured: ${error.message}`);
+}
